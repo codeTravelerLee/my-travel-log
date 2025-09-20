25806892749d568d2c360bef82ca18103af1c3cc
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2025-09-20 06:11:02 +0000

    just copied UI-DESIGN-STEPS.md from https://github.com/burakorkmez/twitter-clone/blob/master/UI-DESIGN-STEPS.md (우선 뼈대는 튜토리얼대로 복붙하고, 나중에 내 것으로 바꿔갈 예정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import SignUpPage from "./pages/auth/signUp/SignUpPage";
+import LogInPage from "./pages/auth/logIn/LogInPage";
 
 function App() {
-  return <>hi</>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signUp" element={<SignUpPage />} />
+      <Route path="/logIn" element={<LogInPage />} />
+    </Routes>
+  );
 }
 
 export default App;
