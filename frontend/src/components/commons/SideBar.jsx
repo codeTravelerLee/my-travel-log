4cb16a922fe76e3aade059a9df2b79ed81c873ad
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2025-09-22 11:15:14 +0000

    sidebar.jsx의 하단부 로그인 정보 뜨는거 mock데이터에서 서버 데이터로 변경

--- frontend/src/components/commons/SideBar.jsx
+++ frontend/src/components/commons/SideBar.jsx
@@ -5,7 +5,7 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { useQueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const Sidebar = () => {
@@ -45,11 +45,8 @@ const Sidebar = () => {
     },
   });
 
-  const data = {
-    fullName: "John Doe",
-    username: "johndoe",
-    profileImg: "/avatars/boy1.png",
-  };
+  //전역 상태에서 데이터 가져오기
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
 
   return (
     <div className="md:flex-[2_2_0] w-18 max-w-52">
@@ -79,7 +76,7 @@ const Sidebar = () => {
 
           <li className="flex justify-center md:justify-start">
             <Link
-              to={`/profile/${data?.userName}`}
+              to={`/profile/${authUser?.userName}`}
               className="flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer"
             >
               <FaUser className="w-6 h-6" />
@@ -87,23 +84,27 @@ const Sidebar = () => {
             </Link>
           </li>
         </ul>
-        {data && (
+        {authUser && (
           <>
             <Link
-              to={`/profile/${data.userName}`}
+              to={`/profile/${authUser.userName}`}
               className="mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full"
             >
               <div className="avatar hidden md:inline-flex">
                 <div className="w-8 rounded-full">
-                  <img src={data?.profileImg || "/avatar-placeholder.png"} />
+                  <img
+                    src={authUser?.profileImg || "/avatar-placeholder.png"}
+                  />
                 </div>
               </div>
               <div className="flex justify-between flex-1">
                 <div className="hidden md:block">
                   <p className="text-white font-bold text-sm w-20 truncate">
-                    {data?.fullName}
+                    {authUser?.fullName}
                   </p>
-                  <p className="text-slate-500 text-sm">@{data?.userName}</p>
+                  <p className="text-slate-500 text-sm">
+                    @{authUser?.userName}
+                  </p>
                 </div>
               </div>
             </Link>
