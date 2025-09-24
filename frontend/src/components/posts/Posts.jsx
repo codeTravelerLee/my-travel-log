8804e760c268cd8b28ebda903cb79549a60ed1f9
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2025-09-24 11:29:37 +0000

    Posts.jsx 개발중... 상단 탭 [모든글보기, 팔로잉 글 보기]의 선택에 따라 서로 다른 경로로 요청하도록 구현중

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -1,9 +1,45 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
 
-const Posts = () => {
-  const isLoading = false;
+const Posts = ({ feedType }) => {
+  const fetchPostEndpoint = () => {
+    switch (feedType) {
+      case "모든 글 보기":
+        return "/api/posts/all";
+      case "팔로잉":
+        return "api/posts/following";
+      default:
+        return "/api/posts/all";
+    }
+  };
+
+  //홈페이지의 상단 탭에서 [모든글 보기, 팔로잉 글 보기]의 값에 따라 각각의 api경로로 요청
+  const POST_ENDPOINT = fetchPostEndpoint();
+
+  const {data, isLoading} = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_SERVER_URI}/${POST_ENDPOINT}`
+        );
+
+        const response = await res.json();
+
+        if (!res.ok || response.error)
+          throw new Error(response.error || "알 수 없는 에러가 발생했습니다!");
+
+        return response; //이게 data로 들어감 
+      } catch (error) {
+        console.error(
+          `error happended while fetching posts...: ${error.message}`
+        );
+        throw error;
+      }
+    },
+  });
 
   return (
     <>
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -4,11 +4,10 @@ import Posts from "../../components/posts/Posts";
 import CreatePost from "../post/CreatePost";
 
 const HomePage = () => {
-  const [feedType, setFeedType] = useState("forYou");
+  const [feedType, setFeedType] = useState("모든 글 보기");
 
   return (
     <>
-      home
       <div className="flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen">
         {/* Header */}
         <div className="flex w-full border-b border-gray-700">
@@ -16,19 +15,19 @@ const HomePage = () => {
             className={
               "flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
             }
-            onClick={() => setFeedType("forYou")}
+            onClick={() => setFeedType("모든 글 보기")}
           >
-            For you
-            {feedType === "forYou" && (
+            모든 글 보기
+            {feedType === "모든 글 보기" && (
               <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
             )}
           </div>
           <div
             className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
-            onClick={() => setFeedType("following")}
+            onClick={() => setFeedType("팔로잉")}
           >
-            Following
-            {feedType === "following" && (
+            팔로잉
+            {feedType === "팔로잉" && (
               <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
             )}
           </div>
