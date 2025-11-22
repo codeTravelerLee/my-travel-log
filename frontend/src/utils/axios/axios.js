5858afc00cd2bd50b2d0b25ad6dc331a23dd481a
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2025-11-22 09:49:20 +0000

    feat: 액세스 토큰 갱신

--- frontend/src/utils/axios/axios.js
+++ frontend/src/utils/axios/axios.js
@@ -10,9 +10,30 @@ const axiosInstance = axios.create({
   withCredentials: true, //cookie
 });
 
+//액세스 토큰 갱신 
 axiosInstance.interceptors.response.use(
   (response) => response,
-  async (error) => {}
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        await axios.post(
+          `${import.meta.env.VITE_SERVER_URI}/api/auth/refreshToken`,
+          {},
+          { withCredentials: true }
+        );
+
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
