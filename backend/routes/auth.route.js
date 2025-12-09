18f4dbb5423c984069e50c7e33a1d3dbd1594cf7
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2025-12-09 03:41:41 +0000

    fix: 액세스토큰 갱신 안되는 오류 해결

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -17,7 +17,7 @@ router.post("/logIn", logIn); //로그인
 router.post("/logOut", protectedRoute, logOut); //로그아웃
 
 //액세스 토큰 갱신
-router.post("/refreshToken", protectedRoute, refreshAccessToken);
+router.post("/refreshToken", refreshAccessToken);
 
 //클라이언트에서 비밀번호 수정시, 현재 비밀번호가 맞는지 확인하는 API
 // router.post("/verifyPassword", protectedRoute, verifyPassword);
